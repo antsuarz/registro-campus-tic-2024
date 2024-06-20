ce6598c2d5626c3dc5f3cb2a975ee35818b405ca
antsuarz <uo270543@uniovi.es>
2024-06-20 21:27:03 +0200

    LogOut implementado

--- frontEnd/scripts/admin.js
+++ frontEnd/scripts/admin.js
@@ -1,9 +1,9 @@
-
+API_URL = 'http://156.35.98.159:8080/'
 checkAuthentication();
 
 function checkAuthentication() {
     console.log("auth");
-    fetch('http://156.35.98.159:8080/authenticated', {
+    fetch(API_URL+'authenticated', {
         method: 'GET',
         credentials: 'same-origin',
         headers: {
@@ -17,10 +17,10 @@ function checkAuthentication() {
             const adminSection = document.getElementById('admin-section');
             adminSection.innerHTML = ''; 
             const paragraph1 = document.createElement('p');
-            paragraph1.textContent = 'Este es un párrafo dinámico agregado cuando está autenticado.';
+            paragraph1.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend consequat interdum. Sed sed dolor vitae sem feugiat scelerisque. Nunc egestas mi odio, sit amet luctus mi euismod ac. Etiam ut dolor ex. Morbi varius diam eros, sit amet vulputate sem vulputate quis. Donec a nulla vitae purus sagittis vulputate. Aenean erat mauris, consectetur at scelerisque eu, faucibus ac eros.';
             adminSection.appendChild(paragraph1); 
             const paragraph2 = document.createElement('p');
-            paragraph2.textContent = 'Otro párrafo dinámico para demostrar la funcionalidad.';
+            paragraph2.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend consequat interdum. Sed sed dolor vitae sem feugiat scelerisque. Nunc egestas mi odio, sit amet luctus mi euismod ac. Etiam ut dolor ex. Morbi';
             adminSection.appendChild(paragraph2);
             return response.json(); 
         } else { 
@@ -29,3 +29,15 @@ function checkAuthentication() {
     })
 }
 
+document.getElementById('logout-link').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    fetch(API_URL+'logOut', {
+        method: 'POST', 
+        credentials: 'same-origin'
+    })
+    .then(response => {
+        if (response.ok) {
+            window.location.href = 'index.html';
+        } 
+    })
+});
