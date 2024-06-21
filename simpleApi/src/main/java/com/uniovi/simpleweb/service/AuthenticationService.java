a896c98a72fad0e6c8122d0d6fa144a9f101d9db
antsuarz <uo270543@uniovi.es>
2024-06-21 23:00:39 +0200

    Cambio de contraseña :-)

--- simpleApi/src/main/java/com/uniovi/simpleweb/service/AuthenticationService.java
+++ simpleApi/src/main/java/com/uniovi/simpleweb/service/AuthenticationService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthenticationService {
     public boolean authenticate(String username, String password) {
-        return "admin".equals(username) && "password".equals(password);
+        return "admin".equals(username) && "18436572".equals(password);
     }
+
 }
